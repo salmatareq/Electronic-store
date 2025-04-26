15621a3e2f0b5db391789de4cf9c05d164bb5396
MarTina Mina <martinaamina05@gmail.com>
2025-04-27 01:06:29 +0300

    Add files via upload
    sec2&sec3_MarTina

---
+++ web-project/public/actoins.js
@@ -0,0 +1,11 @@
+const hidden_menu = document.querySelector(".hide");
+const arrow = document.querySelector(".toclick");
+
+arrow.addEventListener("click", function () {
+  if (hidden_menu.style.display === "block") {
+    hidden_menu.style.display = "none";
+  } else {
+    hidden_menu.style.display = "block";
+  }
+});
+console.log("done");
